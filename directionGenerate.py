c45539c42e6e359786bdef7da79b87d11ef37397
viva <mydotter@hotmail.com>
2021-12-17 16:03:13 +0000

    Implement the multiple direction application

--- directionGenerate.py
+++ directionGenerate.py
@@ -17,10 +17,12 @@ URL_FFHQ = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ'
 
 latentCodeDir = '.'
 generateTarget = '.'
+coefConfigFile = ''
 
 if(len(sys.argv)>1):
     latentCodeDir = sys.argv[1]
     generateTarget = sys.argv[2]
+    coefConfigFile = sys.argv[3]
 else:
     # expect to fail
     pass
@@ -52,7 +54,28 @@ def move_and_show2(latent_vector, direction, coeffs, direction2, coeffs2):
         new_latent_vector = latent_vector.copy()
         new_latent_vector[:8] = (latent_vector + coeff*direction + coeffs2[i]*direction2)[:8]
         generate_image(new_latent_vector).save('%s/%08d.png'%(generateTarget,i))
+
+def move_and_show3(latent_vector, coeffForRequests, directionObjs, i, t):
+    #fig,ax = plt.subplots(1, len(coeffs), figsize=(15, 10), dpi=80)
+    new_latent_vector = latent_vector.copy()
+    for key in coeffForRequests:
+        new_latent_vector[:8] = (new_latent_vector + directionObjs[key] * coeffForRequests[key].GetCoef(t))[:8]
+        
+    generate_image(new_latent_vector).save('%s/%08d.png'%(generateTarget,i))    
+
+def move_and_show4(latent_vector, coeffForRequests, directionObjs):
+    framesCount = 0
+
+    for key in coeffForRequests:
+        framesCount = len(coeffForRequests[key])
+        break
     
+    for i in range(framesCount):
+        new_latent_vector = latent_vector.copy()
+        for key in coeffForRequests:
+            new_latent_vector[:8] = (new_latent_vector + directionObjs[key] * coeffForRequests[key][i])[:8]
+        generate_image(new_latent_vector).save('%s/%08d.png'%(generateTarget,i))    
+
     
 # Loading already learned representations
 for latentcodeName in os.listdir(latentCodeDir):
@@ -61,95 +84,27 @@ for latentcodeName in os.listdir(latentCodeDir):
     latentInstance = np.load(raw_img_path)
     
     # Loading already learned latent directions
-    smile_direction = np.load('%s/ffhq_dataset/latent_directions/smile.npy'%currentPath)
-    gender_direction = np.load('%s/ffhq_dataset/latent_directions/gender.npy'%currentPath)
-    age_direction = np.load('%s/ffhq_dataset/latent_directions/age.npy'%currentPath)
-    angle_horizontal = np.load('%s/ffhq_dataset/latent_directions/angle_horizontal.npy'%currentPath)
-    angle_vertical = np.load('%s/ffhq_dataset/latent_directions/angle_vertical.npy'%currentPath)
-    eyes_open = np.load('%s/ffhq_dataset/latent_directions/eyes_open.npy'%currentPath)
+    directionObjs = {}
+    
+    directionObjs["smile.npy"] = np.load('%s/ffhq_dataset/latent_directions/smile.npy'%currentPath)
+    directionObjs["gender.npy"]  = np.load('%s/ffhq_dataset/latent_directions/gender.npy'%currentPath)
+    directionObjs["age.npy"]  = np.load('%s/ffhq_dataset/latent_directions/age.npy'%currentPath)
+    directionObjs["angle_horizontal.npy"]  = np.load('%s/ffhq_dataset/latent_directions/angle_horizontal.npy'%currentPath)
+    directionObjs["angle_vertical.npy"]  = np.load('%s/ffhq_dataset/latent_directions/angle_vertical.npy'%currentPath)
+    directionObjs["eyes_open.npy"]  = np.load('%s/ffhq_dataset/latent_directions/eyes_open.npy'%currentPath)
     # In general it's possible to find directions of almost any face attributes: position, hair style or color ... 
     # Additional scripts for doing so will be realised soon
 
-    totalFrame = 24 * 4
-    f = -1.0
-    t = 1.5
-    step = (t - f)/totalFrame
-    smileRange = []
-    for x in range(totalFrame):
-        smileRange.append(f)
-        f += step
-
-    totalFrame = 24 * 2
-    f = 1.5
-    t = 1.0
-    for x in range(totalFrame):
-        smileRange.append(f)
-        
-    totalFrame = 24 * 2
-    f = 1.5
-    t = 1.0
-    step = (t - f)/totalFrame
-    for x in range(totalFrame):
-        smileRange.append(f)
-        f += step
-        
-    totalFrame = 24 * 2
-    f = 1.0
-    t = 1.0
-    for x in range(totalFrame):
-        smileRange.append(f)        
-        
-    #move_and_show(latentInstance, smile_direction, smileRange)
+    f=open(coefConfigFile,'rb')  
+    coeffForRequests=pickle.load(f)
     
-    horizontal = []
-    totalFrame = 24 * 5
-    f = -1
-    t = 1.5
-    step = (t - f)/totalFrame
-    for x in range(totalFrame):
-        horizontal.append(f)
-        f += step
 
-    totalFrame = 24 * 5
-    f = 1.5
-    t = 1.0
-    for x in range(totalFrame):
-        horizontal.append(f)
-
-        
-    eyeOpenRange = []
-    totalFrame = 24 * 4
-    f = 0
-    t = 1.0
-    for x in range(totalFrame):
-        eyeOpenRange.append(f)
-        
-    totalFrame = (int)(24 * 0.5)
-    f = 0
-    t = 1.5
-    step = (t - f)/totalFrame
-    for x in range(totalFrame):
-        eyeOpenRange.append(f)
-        f += step
-        
-    totalFrame = (int)(24 * 0.5)
-    f = 1.5
-    t = 0
-    step = (t - f)/totalFrame
-    for x in range(totalFrame):
-        eyeOpenRange.append(f)
-        f += step        
-
-    totalFrame = 24 * 5
-    f = 0
-    t = 1.0
-    for x in range(totalFrame):
-        eyeOpenRange.append(f)
+    move_and_show4(latentInstance,coeffForRequests, directionObjs)
     
     localtime = time.localtime(time.time())
     print(localtime)
     #move_and_show2(latentInstance, smile_direction, smileRange, angle_horizontal, horizontal)
-    move_and_show2(latentInstance, smile_direction, smileRange, eyes_open, eyeOpenRange)
+    #move_and_show2(latentInstance, smile_direction, smileRange, eyes_open, eyeOpenRange)
     
     # we expect only 1 latent code in the folder.
     localtime = time.localtime(time.time())
